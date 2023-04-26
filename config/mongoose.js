0425a03f5a0d0afea9894e529d398663d4f035c1
Rajendra Sarak <rajendrasarak622@gmail.com>
2023-04-26 18:38:41 +0530

    mongoose connected to atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/todo_list_db');
-const db = mongoose.connection;
-// error
-db.on('error',console.error.bind(console,'erroe connecting to db'));
-// up and running then message
-db.once('open',function(){
-    console.log('Successfully connected to the database')
-})
+// mongoose.connect('mongodb://127.0.0.1/todo_list_db');
+// const db = mongoose.connection;
+// // error
+// db.on('error',console.error.bind(console,'erroe connecting to db'));
+// // up and running then message
+// db.once('open',function(){
+//     console.log('Successfully connected to the database')
+// })
+
+
+
+// replace the uri string with your connection string
+const db = "mongodb+srv://rajendrasarak622:2FWW3BrZS2Q8pltl@cluster1.h8vxdpk.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>{
+    console.log('Successfully connected to the database');
+}).catch((err)=> console.log('Failed to connect to the database', err));
